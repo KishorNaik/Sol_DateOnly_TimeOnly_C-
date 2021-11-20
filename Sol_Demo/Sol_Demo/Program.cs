ad2ba9d2a46a699a4d2ab3079911bf9e16136907
Kishor Naik <kishor.naik011.net@gmail.com>
2021-11-20 13:32:54 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//https://www.stevejgordon.co.uk/using-dateonly-and-timeonly-in-dotnet-6
+
+// DateOnly
+var currentDate = DateOnly.FromDateTime(DateTime.Now);
+Console.WriteLine(currentDate);
+
+// TimeOnly
+var currentTime = TimeOnly.FromDateTime(DateTime.Now);
+Console.WriteLine(currentTime);
